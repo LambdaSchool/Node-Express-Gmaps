53091e8f222261e47f1c7fb9b3e43c47ba250022
Igor Yermak <igor.yermak@gmail.com>
2018-03-07 13:33:18 -0500

    Igor Yermak & Russell Stinson: WIP
    installing dependencies and beginning server boilerplate

---
+++ server.js
@@ -0,0 +1,10 @@
+const { PORT, gmaps } = require('/config.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const server = express();
+
+const API_KEY = gmaps.apiKey;
+
+server.use(bodyParser.json());
+
+server.listen(PORT);
