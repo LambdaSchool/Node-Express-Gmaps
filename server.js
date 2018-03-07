673cff0b49e2725ea3719a47dec1b667e8902514
aceredesign <steven.magadan@hotmail.com>
2018-03-07 14:57:11 -0800

    Dixie/Steven - Finished search location and details

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ server.use(bodyParser.json());
 const PORT = config.port;
 const MAP_KEY = config.gmaps.apiKey;
 
-//Put api here
-let results;
-let mapId;
-
 const STATUS_USER_ERROR = 422;
 const STATUS_SUCCESS = 200;
 
@@ -21,41 +17,31 @@ const URL_PLACE_DETAILS = "";
 
 const url = "https://maps.googleapis.com/maps/api/place";
 
+const query = "coffee+shops+in+Austin";
+
 server.get("/place", (req, res) => {
-  const query = "coffee+shops+in+Austin";
-  const search = req.query.search;
+  const { search } = req.query;
   if (!search) {
     res.send({ STATUS_USER_ERROR: "Input a place" });
     return;
   }
   console.log("Your search is ", search);
   fetch(`${url}/textsearch/json?query=${search}&key=${MAP_KEY}`)
-    .then(place => place.json())
+    .then(res => res.json())
+    .then(json => json.results[0].place_id)
     .then(place => {
-      console.log(place);
-      mapId = place.results[0].place_id;
-      res.send(place);
+      fetch(`${url}/details/json?placeid=${place}&key=${MAP_KEY}`)
+        .then(res => res.json())
+        .then(json => {
+          res.send({ STATUS_SUCCESS: json.result });
+        })
+        .catch(err => {
+          res.send({ STATUS_USER_ERROR: "Error fetching place" });
+        });
     })
     .catch(err => {
-      res.status(STATUS_USER_ERROR);
-      res.json({ error: err });
+      res.send({ STATUS_USER_ERROR: "Error fetching details" });
     });
-});
-
-let result;
-
-server.get("/place", (req, res) => {
-  console.log("second map id", mapId);
-  fetch(
-    `https://maps.googleapis.com/maps/api/place/details/json?placeid=${mapId}&key=${MAP_KEY}`
-      .then(place => place.json())
-      .then(place => {
-        res.send(place);
-      })
-      .catch(err => {
-        res.send({ STATUS_USER_ERROR: "There was an error" });
-      })
-  );
 });
 
 server.listen(PORT, err => {
