6bb527781d4b2dd08a534a91acc83825dd9cc5a7
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-07 17:59:50 -0500

    Removed /search from GET endpoint; started working on stretch

--- server.js
+++ server.js
@@ -1,41 +1,58 @@
-const config = require('./config.js');
-const express = require('express')
-const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
+const config = require("./config.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 
 const PORT = config.port;
 const KEY = config.gmaps.apiKey;
-const QUERY = 'coffee+shops+in+Seattle';
+const QUERY = "coffee+shops+in+Seattle";
 const PLACE_SEARCH_URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?`;
 const PLACE_DETAILS_URL = `https://maps.googleapis.com/maps/api/place/details/json?`;
 
 const server = express();
 server.use(bodyParser.json());
 
-server.get('/place/search', (req, res) => {
-  let { term } = req.query
+server.get("/place", (req, res) => {
+  let { term } = req.query;
   let searchResult = {};
-  let placeId = '';
-  console.log(term);
+  let placeId = "";
   fetch(`${PLACE_SEARCH_URL}query=${term}&key=${KEY}`)
     .then(res => res.json())
     .then(json => {
-  searchResult = json.results[0];
-  placeId = searchResult.place_id
-  fetch(`${PLACE_DETAILS_URL}placeid=${placeId}&key=${KEY}`)
-    .then(res => res.json())
-    .then(json =>{
-      console.log(json);
-      res.status(200);
-      res.send(json);
+      searchResult = json.results[0];
+      placeId = searchResult.place_id;
     })
-  })
-})
+    .then(() => {
+      fetch(`${PLACE_DETAILS_URL}placeid=${placeId}&key=${KEY}`)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        res.status(200);
+        res.send(json);
+      });
+    })
+    .catch(err => {
+      console.log(`There was an error: ${err}`)
+    })
+});
 
-server.listen(PORT, (err) => {
+// server.get("/places", (req, res) => {
+//   let { term } = req.query;
+//   let searchResult = {};
+//   let placeIds = [];
+//   fetch(`${PLACE_SEARCH_URL}query=${term}&key=${KEY}`)
+//     .then(res => res.json())
+//     .then(json => {
+//       placeIds = json.results.map((place) => {
+//         return place.place_id;
+//       })
+//     })
+// });
+
+server.listen(PORT, err => {
   if (err) {
     console.log(`There was an error: ${err}`);
   } else {
     console.log(`Server is listening on port ${PORT}`);
   }
-})
+});
