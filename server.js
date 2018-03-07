e5df591ea3ce09f4b2fe3e612d43e5351892a0de
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-07 16:21:40 -0500

    Got both fetch requests working

--- server.js
+++ server.js
@@ -5,21 +5,33 @@ const fetch = require('node-fetch');
 
 PORT = config.port;
 KEY = config.gmaps.apiKey;
-QUERY = 'coffee+shops+in+Brooklyn'
-MAPS_URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${QUERY}&key=${KEY}`
+QUERY = 'coffee+shops+in+Seattle';
+// PLACE_SEARCH_URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${QUERY}&key=${KEY}`;
 
 
 const server = express();
 server.use(bodyParser.json());
 
-let searchResult = {}
-
-fetch(MAPS_URL)
-	.then(res => res.json())
-  .then(json => {
-    searchResult = json;
-    console.log(searchResult);
+  
+server.get('/place/search', (req, res) => {
+let { term } = req.query
+let searchResult = {};
+let placeId = '';
+console.log(term);
+fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${term}&key=${KEY}`)
+.then(res => res.json())
+.then(json => {
+  searchResult = json.results[0];
+  placeId = searchResult.place_id
+  fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${KEY}`)
+    .then(res => res.json())
+    .then(json =>{
+      console.log(json);
+      res.status(200);
+      res.send(json);
+    })
   })
+})
 
 server.listen(PORT, (err) => {
   if (err) {
