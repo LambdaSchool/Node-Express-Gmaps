72e70560887ecc6f639b67537b619b5cda5eb2d4
aceredesign <steven.magadan@hotmail.com>
2018-03-07 11:04:32 -0800

    testing to see if changes commit

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-const express = require('express');
-const  config  = require('./config.js');
+const bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+
+const config = require("./config.js");
 const PORT = config.port;
 
+const STATUS_USER_ERROR = 422;
+const STATUS_SUCCESS = 200;
 
+const URL_PLACE_SEARCH = "";
+const URL_PLACE_DETAILS = "";
 
-/// It should be added. 
+const query = "coffee+shops+in+Austin";
 
+server.use(bodyParser.json());
 
-
-
-
-
-
-
-
-
-
-
-
-server.listen(PORT, err => {
-    if (err) {
-        console.log(`Error starting server: ${err}`);
-    } else {
-        console.log(`App listening on port ${PORT}`)
-    }
-}) 
+server.get("/place", (req, res) => {
+  const { search } = req.query;
+});
