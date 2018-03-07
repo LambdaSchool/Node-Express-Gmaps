e294bb96fb08675b3392573b898a3002c98a34fa
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-07 15:14:38 -0500

    Basic call to API working

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const config = require('./config.js');
 const express = require('express')
 const bodyParser = require('body-parser');
-const fetch = ('node-fetch');
+const fetch = require('node-fetch');
 
 PORT = config.port;
 KEY = config.gmaps.apiKey;
+QUERY = 'coffee+shops+in+Brooklyn'
+MAPS_URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${QUERY}&key=${KEY}`
+
 
 const server = express();
 server.use(bodyParser.json());
+
+let searchResult = {}
+
+fetch(MAPS_URL)
+	.then(res => res.json())
+  .then(json => {
+    searchResult = json;
+    console.log(searchResult);
+  })
 
 server.listen(PORT, (err) => {
   if (err) {
