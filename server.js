3bbd02564986ee1ff4e720c6133a82ac9d2684a3
ashleijo <shuryjo@gmail.com>
2018-03-07 16:30:58 -0500

    adding get method to get response of places

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const nodeFetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 const server = express();
 const STATUS_USER_ERROR = 422;
 const STATUS_SUCCESS = 200;
 const config = require("./config.js");
 
+const url = 'https://maps.googleapis.com/maps/api/place/';
+
 server.use(bodyParser.json());
+
+server.get("/place", (req, res) => {
+  const clientProvided = Object.keys(req.query)[0];
+
+  fetch(`${url}textsearch/json?query=${clientProvided}&key=${config.gmaps.apiKey}`)
+    .then(res => res.json())
+    .then(json => console.log(json))
+})
+
 
 server.listen(config.port, (err) => {
   if (err) {
