aa47fce595cc8bbce373d00ac51eb0a02e3b324f
shobanavv <shobanavv@gmail.com>
2018-03-07 12:42:43 -0800

    Initial setup, commit - fetch returns promise

---
+++ server.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch');
+
+const config = require('./config.js');
+
+const myKey = config.gmaps.apiKey;
+
+
+console.log(fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Chicago&key=${myKey}`)
+    .then(res => res.json())
+    .then(body => console.log("body")));
