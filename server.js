f48cc5d169baca662685ed4db538d76669cb99a5
Christopher Beards <beardscm@gmail.com>
2018-03-07 13:42:19 -0500

    Receiving Response from the API

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const request = require('request');
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const config = require('./config.js');
 const STATUS_USER_ERROR = 422;
+const KEY = config.gmaps.apiKey;
 
 const server = express();
 server.use(bodyParser.json());
 server.use(cors());
+
+const PORT = config.port;
+
 
 //===================================================================================================
 // Create an endpoint `/place` that, provided a query, returns the detailed information about 
 // the first place that is in the array of places returned to you from `Place Search`.
 //===================================================================================================
 
-server.get('/place', () => {
-  request('https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=AIzaSyCKBTRT31op9Mp2Q0VF2ul_SWBf5xCbzp4', (error, response, body) => {
+server.get('/place', (req, res) => {
+  request(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=${KEY}`, (error, response, body) => {
     res.send(body);
   });
 });
@@ -36,7 +40,7 @@ server.get('/places', () => {
 
 server.get('*', (req, res) => {
   res.status(STATUS_USER_ERROR);
-  res.send(console.log("Page not found"))
+  res.send("Page not found");
 });
 
 
