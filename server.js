c40216307ac0ac5ceaadd2b2c44b40b8985e7fcd
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-07 13:45:04 -0600

    Server Boilerplate
    in server.js:
    add requirements
    add consts for status
    initiate var for data
    invoke server
    set up first get and fetch lines

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('connected');
+const bodyParser = require('body-parser');
+const express = require('express');
+const fetch = require('node-fetch'); 
+
+const STATUS_SUCCESS = 200;
+const STATUS_USER_ERROR = 422;
+
+let data = [];
+
+const server = express();
+server.use(bodyParser.json());
+
+server.get('/posts', (req, res) => {
+  const { search } = req.query;
+  res.send();
+});
+
+server.fetch('')
+	.then(res => res.json())
+  .then(json => console.log(json));
