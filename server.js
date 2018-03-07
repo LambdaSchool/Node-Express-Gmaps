0fca6f65e35bd43deae521f487b1daf760caa05f
cliffkang <cliffgkang@gmail.com>
2018-03-07 11:34:14 -0800

    tried the stretch

--- server.js
+++ server.js
@@ -24,16 +24,49 @@ server.get('/place', (req, res) => {
         .then(json => {
           res.status(STATUS_SUCCESSFUL);
           res.send(json.result);
-          console.log('testing');
         })
         .catch(err => {
           res.status(STATUS_USER_ERROR);
-          res.send({ error: "Error fetching place details"});
+          res.send({ error: "Error fetching place details" });
         });
     })
     .catch(err => {
       res.status(STATUS_USER_ERROR);
-      res.send({ error: "Error fetching nearby places"});
+      res.send({ error: "Error fetching nearby places" });
+    });
+});
+
+server.get('/places', (req, res) => {
+  const { placeNames } = req.query;
+  const placesResult = [];
+  fetch(`${url}textsearch/json?query=${placeNames}&key=${API_KEY}`)
+    .then(res => res.json())
+    .then(json => json.results)
+    .then(places => {
+      let newPlace = [];
+      places.forEach(place => {
+        fetch(`${url}details/json?placeid=${place.place_id}&key=${API_KEY}`)
+          .then(res => res.json())
+          .then(json => {
+            newPlace.push(json.result);
+          })
+          .catch(err => {
+            res.status(STATUS_USER_ERROR);
+            res.send({ error: "Error fetching place details" });
+          });
+      })
+      return newPlace
+    })
+    .then((place) => {
+      place.forEach(place => {
+        placesResult.push(place);
+      })
+      res.status(STATUS_SUCCESSFUL);
+      res.send(placesResult);
+    })
+    .catch(err => {
+      res.status(STATUS_USER_ERROR);
+      res.send({ error: "Error fetching nearby places" });
     });
 });
 
