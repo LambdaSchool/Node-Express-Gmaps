fe676de8d2a57e0205330405a78ba7d4bc1aae77
Res Yeg <shaqk702@gmail.com>
2018-03-07 13:16:15 -0800

    changes in server.js

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ const server = express();
 const STATUS_SUCCESSFUL = 200;
 const STATUS_USER_ERROR = 422;
 const url = 'https://maps.googleapis.com/maps/api/place';
+
+server.use(bodyParser.json());
+
+server.listen(PORT, (err) => {
+    if (err) console.error(err);
+    else console.log(`Server is listening on port ${PORT}`);
+});
