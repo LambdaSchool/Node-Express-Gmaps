9ecf3495d0f2ff5fb94d1601b68a14082ea4a834
Christopher Beards <beardscm@gmail.com>
2018-03-07 13:27:51 -0500

    Installed Nodemon, Server Structure Established, config created

--- server.js
+++ server.js
@@ -16,10 +16,23 @@ server.use(cors());
 //===================================================================================================
 
 server.get('/place', () => {
-  request('https://maps.googleapis.com/maps/api/place/textsearch/json?query=', (error, response, body) => {
+  request('https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=AIzaSyCKBTRT31op9Mp2Q0VF2ul_SWBf5xCbzp4', (error, response, body) => {
     res.send(body);
-  })
+  });
 });
+
+//===================================================================================================
+// Create an endpoint `/places` that, provided a query returns the detailed information about ALL 
+// places returned to you from `Place Search`.
+//===================================================================================================
+
+server.get('/places', () => {
+  request('', (error, response, body) => {
+    res.send(body);
+  });
+});
+
+// Catch All
 
 server.get('*', (req, res) => {
   res.status(STATUS_USER_ERROR);
