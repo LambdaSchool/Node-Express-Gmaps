ae0b3c5e01a62c1017376efe1e94a3042d06b80e
Mephestys <mephestys@gmail.com>
2018-03-07 13:37:21 -0700

    Mostly implemented GET - Query from user is passed to Google API, API key added to request. - Currently does nothing with response data.

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const fetch = require('node-fetch'); 
+const fetch = require('node-fetch');
+const config = require("./config.js");
 
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
-let data = [];
+// let data = [];
+const key = config.gmaps.apiKey;
 
 const server = express();
 server.use(bodyParser.json());
 
-server.get('/posts', (req, res) => {
-  const { search } = req.query;
-  res.send();
+server.get('/place', (req, res) => {
+  let { query } = req.query;
+  query = query.replace(/\s/g, '+');
+  let endpoint = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`;
+  fetch(endpoint)
+    .then(res => console.log(res.headers.get('content-type')));
+  res.status(STATUS_SUCCESS);
+  res.send({ status: 'success' });
 });
 
-server.fetch('')
-	.then(res => res.json())
-  .then(json => console.log(json));
+
+
+server.listen(config.port);
