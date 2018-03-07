1c2d5ce7f00fc935ebf282efbcccd77ae614a545
Mephestys <mephestys@gmail.com>
2018-03-07 16:59:10 -0700

    GET now should run async, and returns place data to user. - Removed unused vars - Moved `getPlaceInfo` functionality into server.get - res.send results to user (will appear as JSON in browsers)

--- server.js
+++ server.js
@@ -4,47 +4,40 @@ const fetch = require('node-fetch');
 const config = require("./config.js");
 
 const STATUS_SUCCESS = 200;
-const STATUS_USER_ERROR = 422;
 
 const key = config.gmaps.apiKey;
 
 const server = express();
 server.use(bodyParser.json());
 
+let placeData = null;
+
 server.get('/place', (req, res) => {
-  let id = null;
   let { query } = req.query;
   query = query.replace(/\s/g, '+');
   let endpoint = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`;
+
   fetch(endpoint, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   })
   .then(res => res.json())
-  .then(data => {
-    id = data.results[0].place_id;
-    getPlaceInfo(id);
-  })
-  .catch(error => {
-    console.log('An error ocurred while getting query return');
-  });
-  res.status(STATUS_SUCCESS);
-  res.send({ result: 'success' });
-})
-
-const getPlaceInfo = (id) => {
-  let nextEndpoint = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${id}&key=${key}`;
-  fetch(nextEndpoint, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    })
+  .then(json => json.results[0].place_id)
+  .then(place => {
+    fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place}&key=${key}`)
+      .then(res => res.json())
+      .then(json => {
+        res.status()
+        res.status(STATUS_SUCCESS);
+        res.send(json.result);
+      })
+      .catch(error => {
+        console.error('An error ocurred while getting place information: ', error);
+      })
     .catch(error => {
-      console.log('An error ocurred while getting place information');
-    });
-}
+      console.error('An error ocurred while getting place information: ', error);
+    })
+  })
+})
 
 server.listen(config.port);
