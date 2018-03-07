e3be9d5d998c49f3f0be9206ee1a0c8de6556300
rawalhome@gmail.com <rawalhome@gmail.com>
2018-03-07 13:17:20 -0600

    Dani Tacheny + Punit Rawal: completed place Search..placeID and place details...tying it all together now

--- app.js
+++ app.js
@@ -5,8 +5,24 @@ const config = require('./config.js');
 const key = config.gmaps.apiKey;
 const PORT = config.port;
 
-fetch(
-  `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=${key}`,
-)
+const prom = fetch(
+  `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=${key}`)
   .then(res => res.json())
-  .then(json => console.log(json.results[0]));
+  .then(json =>(json.results[0]));
+  console.log(prom);
+  
+const promResolved = Promise.resolve(prom);  
+promResolved.then( abc => {
+  console.log(abc);
+})
+// console.log(promResolved);
+  
+  
+  
+
+
+
+// console.log(result1);
+// fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${result1.place_id}&key=${key}`)
+// .then(res => res.json())
+//   .then(json =>console.log(json));
