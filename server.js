296abf7540de9d4ef6656e5426380315842396f4
Res Yeg <shaqk702@gmail.com>
2018-03-07 13:14:16 -0800

    added consts in server.js

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const fetch = require('node-fetch');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const PORT = config.port;
+const API_KEY = config.gmaps.apiKey;
+const server = express();
+
+const STATUS_SUCCESSFUL = 200;
+const STATUS_USER_ERROR = 422;
+const url = 'https://maps.googleapis.com/maps/api/place';
