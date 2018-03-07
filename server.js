49bb197f4464ba56ca6ee7e861ee47773e4af0e2
Daniel Lara <endtrippy@gmail.com>
2018-03-07 15:26:09 -0700

    Attained Place_id and placed in results Successfully

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
+const config = require('./config.js');
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
+const key = config.gmaps.apiKey;
 const PORT = 3000;
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
-const results = [
-  {
-    city: 'Sidney',
-    country: 'Australia',
-    phone: 1002003000
-  }
-];
+const resultsId = [];
 
 const server = express();
 server.use((req, res, next) => {
@@ -20,10 +17,29 @@ server.use((req, res, next) => {
 });
 server.use(bodyParser.json());
 
-server.get('/', (req, res) => {
-  res.status(STATUS_SUCCESS);
-  res.send(results);
+server.get('/place', (req, res) => {
+  let { query } = req.query;
+  if (!query) {
+    res.status(STATUS_USER_ERROR);
+    res.send({
+      error: `Can't search for nothing be sure to enter a search term`
+    });
+  }
+  query = query.split(' ').join('+');
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`
+    )
+    .then(({ data }) => {
+      let place_id = data.results[0].place_id;
+      resultsId.push(place_id);
+      res.status(STATUS_SUCCESS);
+      res.send({ place_id });
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR);
+      res.send({ err });
+    });
 });
 
 server.listen(PORT);
-
