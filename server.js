6c3185ccdd6fe468f47fb38cf91c79dce1b9cb06
Nathan Daniel <natdf@icloud.com>
2018-03-07 15:28:49 -0600

    Commit 4 server.js: - added fetch response with the required details

--- server.js
+++ server.js
@@ -11,15 +11,36 @@ const PORT = config.port;
 
 server.get('/place', (req, res) => {
   const textSearch = req.query.textSearch;
-  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${textSearch}&key=${config.gmaps.apiKey}`)
+  fetch(
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${textSearch}&key=${
+      config.gmaps.apiKey
+    }`
+  )
     .then(res => res.json())
     .then(json => {
-      const placeID = json.results[0].place_id
-      fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${config.gmaps.apiKey}`)
-        .then(res => res.json())
-        .then(json => res.send(json))
-  })
+      // const placeID = json.results[0].place_id
+      json.results.forEach(element => {
+        resultsArray.push(element);
+      });
+      resultsArray.forEach(result => {
+        placeIdArr.push(result.place_id);
+      });
+      placeIdArr.forEach(place => {
+        fetch(
+          `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place}&key=${
+            config.gmaps.apiKey
+          }`
+        )
+          .then(res => res.json())
+          .then(json => detailsArray.push(json));
+      });
+    });
+  res.send(detailsArray);
 });
+
+const resultsArray = [];
+const detailsArray = [];
+const placeIdArr = [];
 
 server.listen(PORT, err => {
   if (err) {
