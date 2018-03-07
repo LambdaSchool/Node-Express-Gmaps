ca347da819b49085678802892d5436b626bc72d1
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-07 15:28:47 -0600

    import express and put fetch functionality in server.get()
    - now get requests work with queries

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
 const fetch = require('node-fetch');
+const express = require('express');
 
 const config = require('./config.js');
+const server = express();
 
 const key = config.gmaps.apiKey;
 const PORT = config.port;
 
-const prom = fetch(
-  `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=${key}`)
-  .then(res => res.json())
-  .then(json => json.results[0])
-  .then( forId => fetch(
-    `https://maps.googleapis.com/maps/api/place/details/json?placeid=${forId.place_id}&key=${key}`))
-  .then(res => res.json())
-  .then(json => console.log(json.result));
-  // console.log("this is prom :", prom);
+server.get('/place', (req, res) => {
+	const term = req.query.term;
+  const prom = fetch(
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${term}&key=${key}`,
+  )
+    .then(res => res.json())
+    .then(json => json.results[0])
+    .then(forId =>
+      fetch(
+        `https://maps.googleapis.com/maps/api/place/details/json?placeid=${
+          forId.place_id
+        }&key=${key}`,
+      ),
+    )
+    .then(res => res.json())
+    .then(json => res.send(json.result));
+});
 
-  
-
-
-// fetch(
-//   `https://maps.googleapis.com/maps/api/place/details/json?placeid=${returnedPlaceId}&key=${key}`)
-//   .then(res => res.json())
-//   .then(json => (json.name));
-  // console.log(details);
-  
-  
-  
-  
-
-
-
-// console.log(result1);
-// fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${result1.place_id}&key=${key}`)
-// .then(res => res.json())
-//   .then(json =>console.log(json));
+server.listen(PORT);
