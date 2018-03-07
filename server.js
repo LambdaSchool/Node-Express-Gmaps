062b4d5f8a9b089502d6b4a2dedd57e18e46de7a
Nathan Daniel <natdf@icloud.com>
2018-03-07 13:03:40 -0600

    Commit 3 server.js: - added fetch to server.get - added body-parser

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const config = require('./config.js');
+const bodyParser = require('body-parser');
 
 const server = express();
 
+server.use(bodyParser.json());
+
 const PORT = config.port;
 
+server.get('/place', (req, res) => {
+  const textSearch = req.query.textSearch;
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${textSearch}&key=${config.gmaps.apiKey}`)
+    .then(res => res.json())
+    .then(json => res.send(json))
+});
 
-
-server.listen((PORT, err) => {
+server.listen(PORT, err => {
   if (err) {
     console.log(`ERROR!, ${err}`);
   } else {
