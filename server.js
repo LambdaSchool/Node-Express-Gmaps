9c61143c292c0acbd16090d702682b72a18cc21e
german gonzalez <gcomlnk@gmail.com>
2018-03-07 15:00:00 -0500

    testing search end-point

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
-const express  = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch')
 const config = require('./config');
 
-const port = 3000;
+const port = config.port;
 const server = express();
 const API = config.gmaps.apiKey
 
 server.use(bodyParser.json());
 
 server.get('/place', (req, res) => {
-  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&key=${API}`)
-    .then(res => res.json())
-    .then(json => console.log(json));
+
+    const { searchTxt } = req.query;
+
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${searchTxt}&key=${API}`)
+        .then(res => res.json())
+        .then(json => {
+
+
+            return res.send(json);
+        });
 });
 
+
 server.listen(port, err => {
-  if (err) console.log(err);
-  console.log(`server is listening on port ${port}`);
+    if (err) console.log(err);
+    console.log(`server is listening on port ${port}`);
 });
 
