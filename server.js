954c74882187b8b364df6d5c2c3c7a1865247c46
Daniel Lara <endtrippy@gmail.com>
2018-03-07 16:14:29 -0700

    Single Search Complete // Working on multi result

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = 3000;
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
-const resultsId = [];
+const resultIds = [];
+const results = [];
 
 const server = express();
 server.use((req, res, next) => {
@@ -22,7 +23,7 @@ server.get('/place', (req, res) => {
   if (!query) {
     res.status(STATUS_USER_ERROR);
     res.send({
-      error: `Can't search for nothing be sure to enter a search term`
+      error: `Can't search for nothing be sure to enter something to search.`
     });
   }
   query = query.split(' ').join('+');
@@ -32,13 +33,72 @@ server.get('/place', (req, res) => {
     )
     .then(({ data }) => {
       let place_id = data.results[0].place_id;
-      resultsId.push(place_id);
-      res.status(STATUS_SUCCESS);
-      res.send({ place_id });
+      axios
+        .get(
+          `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${key}`
+        )
+        .then(({ data }) => {
+          let url = data.result.url;
+          let website = data.result.website;
+          let name = data.result.name;
+          res.status(STATUS_SUCCESS);
+          res.send({ name, url, website });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(STATUS_USER_ERROR);
+          res.send({ err: 'WTF' });
+        });
     })
     .catch((err) => {
       res.status(STATUS_USER_ERROR);
       res.send({ err });
+    });
+});
+
+server.get('/places', (req, res) => {
+  let { query } = req.query;
+
+  if (!query) {
+    res.status(STATUS_USER_ERROR);
+    res.send({
+      error: `Can't search for nothing be sure to enter something to search.`
+    });
+  }
+  query = query.split(' ').join('+');
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`
+    )
+    .then(({ data }) => {
+      data.results.map((place) => {
+        resultIds.push(place.place_id);
+      });
+      resultIds.forEach((id) => {
+        axios
+          .get(
+            `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${key}`
+          )
+          .then(({ data }) => {
+            let url = data.result.url;
+            let website = data.result.website;
+            let name = data.result.name;
+            results.push({ name, url, website });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(STATUS_USER_ERROR);
+            res.send({ err: 'WTF' });
+          });
+      });
+    })
+    .then(() => {
+      res.status(STATUS_SUCCESS);
+      res.send({ results });
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR);
+      res.send({ err: 'Something has gone awry' });
     });
 });
 
