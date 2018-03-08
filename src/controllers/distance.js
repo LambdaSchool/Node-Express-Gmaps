0c8bc03b544db0c9dc94afd19bd1175c383c2d1a
Terrie Boeun Kim <terr1e@Terries-iMac.local>
2018-03-08 13:29:23 -0800

    distance model, controller setup in progress

--- src/controllers/distance.js
+++ src/controllers/distance.js
@@ -4,4 +4,20 @@ const router = express.Router();
 const STATUS_USER_ERROR = 422;
 const STATUS_SUCCESS = 200;
 
+const {
+  getDistance
+} = require('../models/distance.js');
+
+router.get('/travel/mode', (req, res) => {
+  getDistance(req.query.search)
+    .then(modes => {
+      res.status(STATUS_SUCCESS);
+      res.send( {results: modes} );
+    })
+    .catch(err => {
+      res.status(STATUS_USER_ERROR);
+      res.send( {error: "Error"} );
+    });
+});
+
 module.exports = router;
