dd20c99d732ef9635e014f5ee0ea6e465df7127a
Mephestys <mephestys@gmail.com>
2018-03-07 18:00:12 -0700

    General syntax and spacing cleanup - Removed 'endpoint' vars to keep code consistent (fetch just gets URL) - Specified method and headers in fetches - Added additional catch block to get(/places) - Added catch block to Promise.all() - Note that searching without specifying a location is unhandled.

--- server.js
+++ server.js
@@ -15,58 +15,66 @@ let placeData = null;
 server.get('/place', (req, res) => {
   let { query } = req.query;
   query = query.replace(/\s/g, '+');
-  let endpoint = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`;
-
-  fetch(endpoint, {
+  
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   })
-  .then(res => res.json())
-  .then(json => json.results[0].place_id)
-  .then(place => {
-    fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place}&key=${key}`)
-      .then(res => res.json())
-      .then(json => {
-        res.status(STATUS_SUCCESS);
-        res.send(json.result);
+    .then(res => res.json())
+    .then(json => json.results[0].place_id)
+    .then(place => {
+      fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place}&key=${key}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
       })
+        .then(res => res.json())
+        .then(json => {
+          res.status(STATUS_SUCCESS);
+          res.send(json.result);
+        })
       .catch(error => {
         console.error('An error ocurred while getting place information: ', error);
-      })
+        })
     .catch(error => {
       console.error('An error ocurred while getting place information: ', error);
-    })
-  })
-})
+    });
+  });
+});
 
 server.get('/places', (req, res) => {
   let { query } = req.query;
   query = query.replace(/\s/g, '+');
-  let endpoint = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`;
-  fetch(endpoint, {
+
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${key}`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   })
-  .then(res => res.json())
-  .then(json => json.results)
-  .then(places => {
-    const promises = [];
-    places.forEach(place => {
-      promises.push(new Promise(resolve => {
-        fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place.place_id}&key=${key}`)
-        .then(res => res.json())
-        .then(json => {
-          resolve(json.result);
-        })
-        .catch(error => {
-          console.error('An error ocurred while getting place information: ', error);
-        });
-      }));
+    .then(res => res.json())
+    .then(json => json.results)
+    .then(places => {
+      const promises = [];
+      places.forEach(place => {
+        promises.push(new Promise(resolve => {
+          fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place.place_id}&key=${key}`)
+            .then(res => res.json())
+            .then(json => {
+              resolve(json.result);
+            })
+          .catch(error => {
+            console.error('An error ocurred while getting places information: ', error);
+          });
+        }));
+      })
+    .catch(error => {
+      console.error('An error ocurred while getting places information: ', error);
+    })
+  Promise.all(promises)
+    .then(places => {
+      res.send(places);
+    })
+    .catch(error => {
+      console.error('An error ocurred with all promises: ', error);
     });
-    Promise.all(promises)
-      .then(places => {
-        res.send(places);
-      });
   });
 });
 
