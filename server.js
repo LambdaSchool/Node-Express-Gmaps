6ae895d90b6ea6f4a9402e8aec943f731bde3969
Peter Gray <peter@petergraycreative.com>
2018-03-07 19:09:18 -0600

    Able to iterate over results and add a promises to the promises variable

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
 const express = require('express');
 const config = require('./config.js');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 const server = express();
 
 server.listen(config.port);
 
-// server.get('./', (req, res) => {
-//     fetch('https://maps.googleapis.com/maps/api/place/textsearch/json', req.body, {key: config})
-//         .then(res => res.json())
-//         .then(json => res.send(json));
-// })
 server.get('/place', (req, res) => {
-    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.query.location}&key=${config.gmaps.apiKey}`)
-        .then(res => res.json())
-        .then(json => {
-            return res.send(json.results[0]);
-        })
-        .catch(err => console.error(err));
-})
+  fetch(
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${
+      req.query.location
+    }&key=${config.gmaps.apiKey}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      return res.status(200), res.send(json.results[0]);
+    })
+    .catch((err) => console.error(err));
+});
 
 server.get('/places', (req, res) => {
-    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.query.location}&key=${config.gmaps.apiKey}`)
-        .then(res => res.json())
-        .then(json => {
-            return res.send(json.results);
-        })
-        .catch(err => console.error(err));
-})
+  let promises;
+  fetch(
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${
+      req.query.location
+    }&key=${config.gmaps.apiKey}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      promises = json.results.map((result) => {
+        return fetch(
+          `https://maps.googleapis.com/maps/api/place/details/json?placeid=${
+            result.id
+          }&key=${config.gmaps.apiKey}`
+        )
+          .then((res) => {
+            return res;
+          })
+          .catch((err) => console.error(err));
+      });
+    })
+    .catch((err) => console.error(err));
+
+  Promise.all(promises)
+    .then((responses) => {
+      return res.status(200), res.send(responses);
+    })
+    .catch((err) => console.error(err));
+});
