02eb4eb92caafcc18dda47af8a8a41b42df0a483
shobanavv <shobanavv@gmail.com>
2018-03-07 16:21:18 -0800

    Getting places and details of places

--- server.js
+++ server.js
@@ -1,10 +1,64 @@
 const fetch = require('node-fetch');
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const config = require('./config.js');
 
 const myKey = config.gmaps.apiKey;
+const PORT = config.port;
 
+const server = express();
 
-console.log(fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Chicago&key=${myKey}`)
+server.use(bodyParser.json());
+
+server.get('/place',(req,res) =>{
+    const placeName = req.query.search;
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${placeName}&key=${myKey}`)
     .then(res => res.json())
-    .then(body => console.log(json.results.name)));
+    .then(place => {
+            fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place.results[0].place_id}&key=${myKey}`)
+            .then(res => res.json())
+            .then(detail => {
+                // console.log(detail);
+                res.status(200);
+                res.send({detail});
+            })
+
+            .catch(err => {
+                res.status(422);
+                res.send({error:"error occured on detail"});
+            } )
+    })
+    .catch(err =>{
+        res.status(422);
+        res.send({error:"error occured"});
+});
+    //return 
+})
+
+// server.get(‘/place/id’,(req,res) =>{
+
+//   const placeDetails = req.query;
+
+//   fetch(`https://maps.googleapis.com/maps/api/place/details/json?query=${placeName}&key=${myKey}`)
+
+//   .then(res => res.json())
+
+//   .then(body =>body.results)
+
+//   .catch(err =>{
+
+//       res.status(422);
+
+//       res.send({error:“error occured”});
+
+// });
+
+// })
+
+server.listen(PORT, err => {
+    if(err) {
+        console.log("There was an error");
+    } else {
+        console.log(`Server is listening on PORT number ${PORT}`);
+    }
+});
