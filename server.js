6bd969e87af71375702de0574f585c3bb382f41a
rcs784 <rcstinson@hotmail.com>
2018-03-08 16:40:12 -0500

    Day 1 complete and corrected

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const server = express();
 const PORT = port;
 
 const API_KEY = gmaps.apiKey;
+const DISTANCE_API_KEY = gmaps.distanceApiKey;
 
 const STATUS_USER_ERROR = 422;
 
@@ -15,8 +16,15 @@ server.use(bodyParser.json());
 server.get('/place', (req, res) => {
   if (req.query.term) {
     fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.query}&key=${API_KEY}`).then((data) => {
+      return JSON.parse(data)[0];
+    })
+    .then((placeID) => {
+      return fetch(`https://maps.googleapis.com/maps/api/place/details/json?key=${API_KEY}&placeid=${placeID}`)
+        .then(data => data);
+    })
+    .then((details) => {
       res.status(200);
-      res.json(JSON.parse(data)[0]);
+      res.send(details);
     })
     .catch((err) => {
       res.status(STATUS_USER_ERROR);
@@ -28,8 +36,26 @@ server.get('/place', (req, res) => {
 server.get('/places', (req, res) => {
   if (req.query.term) {
     fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.query}&key=${API_KEY}`).then((data) => {
+      return JSON.parse(data);
+    })
+    .then((placeIDs) => {
+      const placeDetailsArr = [], promiseArr = [];
+      placeIDs.forEach((id) => {
+        const promise = new Promise(resolve, reject);
+        fetch(`https://maps.googleapis.com/maps/api/place/details/json?key=${API_KEY}&placeid=${id}`).then(details => {
+          placeDetailsArr.push(details);
+          promise.resolve();
+        });
+        promiseArr.push(promise);
+      });
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR);
+      res.send('Error fetching places.');
+    });
+    Promise.all(promiseArr).then(() => {
       res.status(200);
-      res.send(data);
+      res.json(placeDetailsArr);
     })
     .catch((err) => {
       res.status(STATUS_USER_ERROR);
