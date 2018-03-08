ac86529530b92ef662e261f30ceb2b26a4b5dca4
Smithface <kylewibblesmith@gmail.com>
2018-03-08 16:00:36 -0500

    created skeleton for Day2 assignment

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const config = require('../config.js');
+// const distanceController = require('./controllers/distance.js');
+
+const app = express();
+const PORT = config.port;
+
+// app.use(distanceController);
+
+app.listen(PORT, err => {
+  if (err) {
+    console.error('Error starting server:', err);
+  } else {
+    console.log('App listening on port:', PORT);
+  }
+});
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const server = express();
 const STATUS_USER_ERROR = 422;
 const STATUS_SUCCESS = 200;
 const config = require("./config.js");
+const KEY_GMAPS_PLACE = config.gmaps.apiKeys.place;
 
 const url = 'https://maps.googleapis.com/maps/api/place/';
 
@@ -14,10 +15,10 @@ server.use(bodyParser.json());
 server.get("/place", (req, res) => {
   const clientProvided = Object.keys(req.query)[0];
 
-  fetch(`${url}textsearch/json?query=${clientProvided}&key=${config.gmaps.apiKey}`)
+  fetch(`${url}textsearch/json?query=${clientProvided}&key=${KEY_GMAPS_PLACE}`)
     .then(res => res.json())
     .then(json => {
-      fetch(`${url}details/json?placeid=${json.results[0].place_id}&key=${config.gmaps.apiKey}`)
+      fetch(`${url}details/json?placeid=${json.results[0].place_id}&key=${KEY_GMAPS_PLACE}`)
       .then(res => res.json())
       .then(json => res.send(json.result))
       .catch(err => res.send(err))
@@ -29,13 +30,13 @@ server.get("/places", (req, res) => {
   const clientProvided = Object.keys(req.query)[0];
   const resultArray = [];
   const promiseArray = [];
-  const prom = fetch(`${url}textsearch/json?query=${clientProvided}&key=${config.gmaps.apiKey}`);
+  const prom = fetch(`${url}textsearch/json?query=${clientProvided}&key=${KEY_GMAPS_PLACE}`);
   promiseArray.push(prom);
   prom
     .then(res => res.json())
     .then(json => {
       json.results.forEach((elem, i) => {
-        const prom2 = fetch(`${url}details/json?placeid=${elem.place_id}&key=${config.gmaps.apiKey}`)
+        const prom2 = fetch(`${url}details/json?placeid=${elem.place_id}&key=${KEY_GMAPS_PLACE}`)
         promiseArray.push(prom2);
         prom2
           .then(res => res.json())
