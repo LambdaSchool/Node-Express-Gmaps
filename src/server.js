172dcbc8206f51c84fb09bc336a1659771b6ca72
Glenn-David Daniel <d2rd@outlook.com>
2018-03-08 09:30:04 -0800

    Glenn-David solo work.  Partner  never contributed.  Must have submitted his own repo.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-config = require("./config.js");
+config = require('../config.js');
+
 const express = require('express');
-const fetch = require('fetch');
 const bodyParser = require('body-parser');
+const server = express();
+
+const PORT = 3030;
 const STATUS_SUCCESS = 200; //OK
 const STATUS_USER_ERROR = 422; 
-
-const server = express();
 
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
@@ -24,15 +25,37 @@ let places = [
 
   }
 ];
-let queryInput = null; // stores last user query "coffee+shops+in+Austin"
+let placeRequested = null; // stores last user query "coffee+shops+in+Austin"
 let placeResponse = null; // stores webserver responses
 let placeDetails = null;  // stores details on place
 
 // handle user's query request
-
+server.get("/", (req, res) => {
+  if (req.query.placeRequested) {
+    let place = null; //declares empty variable for single item 
+    Object.keys(SSL_OP_LEGACY_SERVER_CONNECT).forEach((id => {
+      if (places[id] === req.query.placeRequested) {
+        place = id;
+      };
+    }));
+    res.status(STATUS_SUCCESS);
+    res.send(place);
+  } else {
+    res.status(STATUS_SUCCESS);
+    res.send(places);
+  }
+});
 
 
 // fetch details on results of users query request
 // FETCH SYNTAX
+//  https://maps.googleapis.com/maps/api/place/nearbysearch/json?[parameter1]&[param2]&[param3]
 
-
+ //TELL THE SERVER TO LISTEN TO 'PORT'
+ server.listen(PORT,(err) => {
+  if(err) {
+    console.log('There was an error starting the server: ${err}');
+  } else {
+    console.log('Server is listening on port ${PORT}');
+  }
+});
