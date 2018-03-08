91696b8f2eefa187a1f3e574d729626d2a673159
Lo Saephan <teamsky916@gmail.com>
2018-03-07 17:15:29 -0800

    refactor places api to using async await to fetch all places details

--- server.js
+++ server.js
@@ -18,51 +18,43 @@ const PORT = config.port;
 // the first place that is in the array of places returned to you from `Place Search`.
 //===================================================================================================
 
-
 server.get('/place', (req, res) => {
   const { search } = req.query;
   request(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${search}&key=${KEY}`, (error, response, body) => {
-
     const parsedResults = JSON.parse(body);
-
     const { place_id } = parsedResults.results[0];
-
     request(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${KEY}`, (error, response, body) => {
       res.send(body);
     });
   });
 });
 
-//===================================================================================================
-// Create an endpoint `/places` that, provided a query returns the detailed information about ALL 
-// places returned to you from `Place Search`.
-//===================================================================================================
+// //===================================================================================================
+// // Create an endpoint `/places` that, provided a query returns the detailed information about ALL 
+// // places returned to you from `Place Search`.
+// //===================================================================================================
+
+async function fetchDetails(results){
+  const details = [];
+  for(let i = 0; i < results.length; i++){
+    const response = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${results[i].place_id}&key=${KEY}`)
+    const data = await response.json();
+    details.push(data.result);
+  }
+  return details;
+}
 
 server.get('/places', (req, res) => {
-
-   const { search } = req.query;
-
-  request(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${search}&key=${KEY}`, (error, response, body) => {
-
-    const parsedResults = JSON.parse(body);
-    const details = [];
-    const listLength = parsedResults.results.length;
-    let counter = 0;
-
-    parsedResults.results.forEach((place) => {
-      request(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place.place_id}&key=${KEY}`, (error, response, body) => {
-        details.push(body);
-        counter++;
-        if(listLength === counter){
-          res.send(details);
-        }
-      });
-    });
-  });
+  const { search } = req.query;
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${search}&key=${KEY}`)
+  .then(res => res.json())
+  .then((places) => { 
+    const promise = fetchDetails(places.results);
+    promise.then(details => res.send(details));
+  })
 });
 
 // Catch All
-
 server.get('*', (req, res) => {
   res.status(STATUS_USER_ERROR);
   res.send("Page not found");
@@ -76,6 +68,3 @@ server.listen(3030, (err) => {
     console.log(`Server is listening on port ${PORT}`)
   }
 });
-
-   
-
