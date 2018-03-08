b6cbf89463a1f2541fba994ba3e2dc464a75ddaa
itsdevinci <devinbaldwin315@gmail.com>
2018-03-07 20:46:40 -0800

    Server -Semi-perfect

--- server.js
+++ server.js
@@ -9,19 +9,41 @@ const server = express();
 
 const STATUS_SUCCESSFUL = 200;
 const STATUS_USER_ERROR = 422;
-const url = 'https://maps.googleapis.com/maps/api/place';
+const detailsUrl = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
+const searchUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
 
 server.use(bodyParser.json());
 
 server.get('/place', (req, res) => {
-<<<<<<< HEAD
 
-});
-=======
-    const { place } 
-})
->>>>>>> df65725e283098d7dfd37597c7f4810ccb13373a
+    const search = req.query.search;
+        console.log(search);
+        const textSearch = searchUrl + search + `&key=${API_KEY}`;
+        console.log(textSearch);
+    
 
+    fetch(textSearch)
+        .then(res => res.json())
+        .then(place => {
+            
+            const results = place.results[0].place_id;
+                
+                fetch(detailsUrl + results + `&key=${API_KEY}`)
+                    .then(res => res.json())
+                    .then(details => {
+                        res.status(STATUS_SUCCESSFUL);
+                        res.send(details);
+                    })
+                    
+                    
+
+         })
+        .catch((err) => {
+            res.status(STATUS_USER_ERROR);
+            res.send({ err });
+
+        });
+    })    
 server.listen(PORT, (err) => {
     if (err) console.error(err);
     else console.log(`Server is listening on port ${PORT}`);
