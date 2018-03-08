5f5a70c4e63e377944662dcb67ed7000c3e8118a
Peter Gray <peter@petergraycreative.com>
2018-03-08 15:57:19 -0600

    getDetails to methods that gets details for a specific result
    Returns a promise to the route when the id (or ids) details are fetched

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const config = require('../config.js');
-const placesController = require('.controllers/places.js');
+const placesController = require('./controllers/places.js');
 
 const app = express();
 const PORT = config.port;
--- src/models/places.js
+++ src/models/places.js
@@ -1,41 +1,42 @@
 const fetch = require('node-fetch');
 const config = require('../../config.js');
 
-const KEY_GMAPS_PLACES = config.gmaps.apiKey;  
-const URI_TEXT_SEARCH = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
-const URI_PLACE_DETAILS = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
+const KEY_GMAPS_PLACES = config.gmaps.apiKey;
+const URI_TEXT_SEARCH =
+  'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
+const URI_PLACE_DETAILS =
+  'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
 
 // Functions go here
-function getIds(query){
+function getIds(query) {
   return new Promise((resolve, reject) => {
     const searchUrl = URI_TEXT_SEARCH + query + '&key=' + KEY_GMAPS_PLACES;
-  fetch(searchUrl)
-    .then(places => places.json())
-    .then((places => places.results.map(place => place.place_id))
-      .then(ids => {
-        resolve(ids);
-      })
-      .catch((err) => {
-        reject(err);
-     });
-  );
+    fetch(searchUrl)
+      .then((places) => places.json())
+      .then(
+        ((places) => places.results.map((place) => place.place_id))
+          .then((ids) => {
+            resolve(ids);
+          })
+          .catch((err) => {
+            reject(err);
+          })
+      );
+  });
 }
 
-
-  fetch()
-    .then((res) => res.json())
-    .then((json) => {
-      const promises = json.results.map((result) => {
-          return fetch()
-            .then((res) => res.json())
-            .catch((err) => console.error(err))
-      });
-
-      Promise.all(promises)
-        .then((responses) => {
-          res.status(200);
-          res.send(responses);
-        })
-        .catch((err) => console.error(err));
-    })
-    .catch((err) => console.error(err));
+function getDetails(ids) {
+  return new Promise((resolve, reject) => {
+    const promises = ids.map((id) => {
+      const searchUrl = URI_PLACE_DETAILS + id + '&key=' + KEY_GMAPS_PLACES;
+      return fetch(searchUrl)
+        .then((details) => details.json())
+        .then((details) => details.result);
+    });
+    Promise.all(promises)
+      .then((details) => {
+        resolve(details);
+      })
+      .catch((err) => reject(err));
+  }).catch((err) => console.error(err));
+}
