b64fb3c402d542367fdd1a3ef4e00ee33e4fee90
Ronnie Miksch <fakepreferences@gmail.com>
2018-03-08 16:50:36 -0800

    Stretch complete, distance returned with place if client provides an origin

--- controllers/places.js
+++ controllers/places.js
@@ -5,52 +5,60 @@ const config = require('../config.js');
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
-const {
-  getIds,
-  getDetails,
-  getDistances,
-} = require('../models/places.js');
+const { getIds, getDetails, getDistances } = require('../models/places.js');
 
 router.get('/place', (req, res) => {
-  const { placeName } = req.query;
-  getIds(placeName)
-    .then(ids => [ids[0]])
-    .then(getDetails)
-    .then(json => {
-      res.status(STATUS_SUCCESS);
-      res.send({ places: json });
-    })
-    .catch(err => {
-      res.status(STATUS_USER_ERROR);
-      res.send({ error: err });
-    });
+    const { placeName, origin } = req.query;
+    getIds(placeName)
+        .then(ids => [ids[0]])
+        .then(getDetails)
+        .then(json => {
+            if (origin) {
+                getDistances(origin, json[0].name)
+                    .then(dist => {
+                        res.status(STATUS_SUCCESS);
+                        res.send({ distances: dist, places: json });
+                    })
+                    .catch(err => {
+                        res.status(STATUS_USER_ERROR);
+                        res.send({ error: err });
+                    });
+            } else {
+                res.status(STATUS_SUCCESS);
+                res.send({ places: json });
+            }
+        })
+        .catch(err => {
+            res.status(STATUS_USER_ERROR);
+            res.send({ error: err });
+        });
 });
 
 router.get('/places', (req, res) => {
-  const { placeNames } = req.query;
-  getIds(placeNames)
-    .then(getDetails)
-    .then(json => {
-      res.status(STATUS_SUCCESS);
-      res.send({ places: json });
-    })
-    .catch(err => {
-      res.status(STATUS_USER_ERROR);
-      res.send({ error: err });
-    });
+    const { placeNames } = req.query;
+    getIds(placeNames)
+        .then(getDetails)
+        .then(json => {
+            res.status(STATUS_SUCCESS);
+            res.send({ places: json });
+        })
+        .catch(err => {
+            res.status(STATUS_USER_ERROR);
+            res.send({ error: err });
+        });
 });
 
 router.get('/travel/mode', (req, res) => {
-  const { origins, destinations } = req.query;
-  getDistances(origins, destinations)
-  .then(json => {
-    res.status(STATUS_SUCCESS);
-    res.send({ distances: json });
-  })
-  .catch(err => {
-    res.status(STATUS_USER_ERROR);
-    res.send({ error: err });
-  });
+    const { origins, destinations } = req.query;
+    getDistances(origins, destinations)
+        .then(json => {
+            res.status(STATUS_SUCCESS);
+            res.send({ distances: json });
+        })
+        .catch(err => {
+            res.status(STATUS_USER_ERROR);
+            res.send({ error: err });
+        });
 });
 
 module.exports = router;
