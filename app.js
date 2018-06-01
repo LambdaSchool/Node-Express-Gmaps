c4cee835ff1f59b04512a1f06f592c9ce7164c46
bbenefield89 <bsquared18@gmail.com>
2018-06-01 12:45:59 -0400

    feat(app.js): returns first location based on a user-provided query string

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = 5000;
+const apiKey = require('./config');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const { query: { q } } = req;
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${ q }&key=${ apiKey }`)
+    .then(res => res.json())
+    .then(json => {
+      res.json(json.results[0])
+    })
+    .catch(err => {
+      console.log('error');
+      res.json(err)
+    });
+});
+
+app.listen(port, () => console.log(`Server listening on port: ${ port }`));
