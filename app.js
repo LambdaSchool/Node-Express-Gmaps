ccf21f70dcd7f7e6b5846a46905231bcaee49f9c
bbenefield89 <bsquared18@gmail.com>
2018-06-01 12:49:29 -0400

    feat(app.js): returns paginated results from GET:/all

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const fetch = require('node-fetch');
 const app = express();
 const port = 5000;
 const apiKey = require('./config');
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   const { query: { q } } = req;
   fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${ q }&key=${ apiKey }`)
     .then(res => res.json())
-    .then(json => {
-      res.json(json.results[0])
-    })
-    .catch(err => {
-      console.log('error');
-      res.json(err)
-    });
+    .then(json => res.json(json.results[0]))
+    .catch(err => res.json(err));
+});
+
+app.get('/all', (req, res) => {
+  const { query: { q } } = req;
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${ q }&key=${ apiKey }`)
+    .then(res => res.json())
+    .then(json => res.json(json.results))
+    .catch(err => res.json(err));
 });
 
 app.listen(port, () => console.log(`Server listening on port: ${ port }`));
